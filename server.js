2a5dd1b0a139aff9fcb6effb32e5eca8ced92b7d
shyamasundar97 <shyamasundar97@gmail.com>
2025-06-07 18:11:49 +0530

    changes

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ const Shift = require('./models/Shift');
 const Attendance = require('./models/Attendance');
 const app = express();
 app.use(bodyParser.json());
-
+const lodash = require('lodash')
 const FAILED = 'FAILED'
 const SUCCESS = 'SUCCESS'
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:4200', // replace with your frontend domain
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+}));
 mongoose.connect('mongodb://localhost:27017/pfm_db', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected!'))
   .catch((err) => console.error('MongoDB connection error:', err));
@@ -48,22 +54,40 @@ app.post('/attendance', async (req, res) => {
     const user = await Attendance.create(saveData);
     res.status(200).json(user);
   } catch (error) {
-      if (error['code'] === 11000) {
-      res.status(400).json({ status: FAILED, error: "alreadyExits",keyValue:error['keyValue']  });
+    if (error['code'] === 11000) {
+      res.status(400).json({ status: FAILED, error: "alreadyExits", keyValue: error['keyValue'] });
     } else {
       res.status(400).json({ error: error.message });
     }
   }
 })
 
+
+app.post('/attendanceEntry', async (req, res) => {
+  try {
+    const { saveData } = req.body
+    const saveResponse = await Attendance.create(saveData);
+
+    res.status(200).json({ status: SUCCESS, records: saveResponse });
+  } catch (error) {
+    if (error['code'] === 11000) {
+      res.status(400).json({ status: FAILED, error: "alreadyExits", keyValue: error['keyValue'] });
+    } else {
+      res.status(400).json({ error: error.message });
+    }
+  }
+})
+
+
 app.post('/shiftCreation', async (req, res) => {
   try {
     const { saveData } = req.body
     const saveResponse = await Shift.create(saveData);
+
     res.status(200).json({ status: SUCCESS, records: saveResponse });
   } catch (error) {
-       if (error['code'] === 11000) {
-      res.status(400).json({ status: FAILED, error: "alreadyExits",keyValue:error['keyValue']  });
+    if (error['code'] === 11000) {
+      res.status(400).json({ status: FAILED, error: "alreadyExits", keyValue: error['keyValue'] });
     } else {
       res.status(400).json({ error: error.message });
     }
@@ -77,7 +101,7 @@ app.post('/staffCreation', async (req, res) => {
     res.status(200).json({ status: SUCCESS, records: saveResponse });
   } catch (error) {
     if (error['code'] === 11000) {
-      res.status(400).json({ status: FAILED, error: "alreadyExits",keyValue:error['keyValue']  });
+      res.status(400).json({ status: FAILED, error: "alreadyExits", keyValue: error['keyValue'] });
     } else {
       res.status(400).json({ error: error.message });
     }
@@ -87,8 +111,8 @@ app.post('/staffCreation', async (req, res) => {
 app.get('/staffList', async (req, res) => {
   try {
     const { filter } = req.query
-    const staffList = await Staff.find().populate('role').populate('department').exec(); 
-    res.status(200).json(staffList);
+    const staffList = await Staff.find().populate('role').populate('department').exec();
+    res.status(200).json({status:SUCCESS,records:staffList});
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -97,8 +121,23 @@ app.get('/staffList', async (req, res) => {
 app.get('/shiftList', async (req, res) => {
   try {
     const { filter } = req.query
-    const shiftList = await Shift.find(filter);
-    res.status(200).json(shiftList);
+    const shiftList = await Shift.find().exec();
+
+    const shiftListJSON = JSON.parse(JSON.stringify(shiftList))
+    const staff = shiftListJSON.map(el => el['staff']);
+    const uniqStaff = lodash.uniq(lodash.flatten(staff))
+    // const obj_ids = uniqStaff.map(function(id) { return ObjectId(id); });
+    const staffList = await Staff.find({ _id: { $in: uniqStaff } });
+    const staffListJSON = JSON.parse(JSON.stringify(staffList))
+    console.log(staffListJSON)
+    shiftListJSON.forEach(eachShift => {
+      eachShift['staffData'] = []
+      eachShift['staff'].forEach(eachStaff => {
+      const data = staffListJSON.find(e=> e['_id'] === eachStaff) 
+        eachShift['staffData'].push(data)
+      })
+    })
+    res.status(200).json({status:SUCCESS,records:shiftListJSON});
   } catch {
     res.status(400).json({ error: 'Error' });
   }
